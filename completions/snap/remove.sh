4c4f7c6fdf65bcb4826c28808b08c32c0c708e8e
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-05-23 17:52:48 -0600

    Added completions for `snap remove`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

---
+++ completions/snap/remove.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+# @flag --no-wait       Do not wait for the operation to finish but just print the change id.
+# @option --revision    Remove only the given revision
+# @flag --purge         Remove the snap without saving a snapshot of its data
+# @flag --terminate     Terminate running processes associated with a snap before removal
+# @arg snap+
+remove() {
+    :;
+}
+
+command eval "$(argc --argc-eval "$0" "$@")"

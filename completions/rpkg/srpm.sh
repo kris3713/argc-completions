334a31431885e5fdc8bef7171ef0c13ffee81b2f
kris3713 <07e5297d5b@c0x0.com>
2025-08-11 18:20:23 -0600

    Added completions for `rpkg srpm`
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

---
+++ completions/rpkg/srpm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+# @flag -h --help               show this help message and exit
+# @option --target              Prep for a specific arch or arch-vendor-os.
+# @option --with <OPTION>       Enable %bcond spec OPTION.
+# @option --without <OPTION>    Disable %bcond spec OPTION.
+# @option --outdir              Where to put any generated rpm content.
+# @option --spec                An input spec template.
+srpm() {
+    :;
+}
+
+command eval "$(argc --argc-eval "$0" "$@")"

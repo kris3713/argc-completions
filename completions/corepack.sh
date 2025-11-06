4dcb1f533721a280fe8de1116f44f6cc43fc5563
kris3713 <07e5297d5b@c0x0.com>
2025-11-05 20:42:02 -0700

    Added completions for `corepack`
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

---
+++ completions/corepack.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+
+
+# {{ corepack disable
+# @cmd
+# @option --install-directoryWhere the shims are located  0
+disable() {
+    :;
+}
+# }} corepack disable
+
+# {{ corepack enable
+# @cmd
+# @option --install-directoryWhere the shims are to be installed  0
+enable() {
+    :;
+}
+# }} corepack enable
+
+# {{ corepack install
+# @cmd
+install() {
+    :;
+}
+# }} corepack install
+
+# {{ corepack pack
+# @cmd
+# @flag --json    If true, the path to the generated tarball will be printed on stdout
+# @option -o --outputWhere the tarball should be generated; by default "corepack.tgz"  0
+pack() {
+    :;
+}
+# }} corepack pack
+
+# {{ corepack up
+# @cmd
+up() {
+    :;
+}
+# }} corepack up
+
+command eval "$(argc --argc-eval "$0" "$@")"

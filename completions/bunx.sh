60c99c48f008b3692dc8fb4ed893dd23c9fcb858
kris3713 <07e5297d5b@c0x0.com>
2026-03-09 12:55:53 -0600

    Added completions for `bunx`
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

---
+++ completions/bunx.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+# @flag --bun                       Force the command to run with Bun instead of Node.js
+# @option -p --package <package>    Specify package to install when binary name differs from package name
+# @flag --no-install                Skip installation if package is not already installed
+# @flag --verbose                   Enable verbose output during installation
+# @flag --silent                    Suppress output during installation
+# @arg package-version <<package><@version>>
+
+command eval "$(argc --argc-eval "$0" "$@")"
